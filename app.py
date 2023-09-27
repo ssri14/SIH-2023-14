d8bab91e6221c87e112f8cb348cd4a5c3fa8fede
palSagnik <112897062+palSagnik@users.noreply.github.com>
2023-09-28 04:12:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,85 +1,25 @@
 import pickle
 import streamlit as st
-import instaloader
-import json
-import csv
-import sys
-import shutil
 import numpy as np
 import time
-
+from sklearn.model_selection import RandomizedSearchCV, train_test_split
+from scipy.stats import randint
 
 pickle_in = open("classifier.pkl","rb")
 model = pickle.load(pickle_in)
 
-def scrap(user):
-    usrname = user
-    ig=instaloader.Instaloader()
+def make_a_dict(FollowerCount, FollowingCount, BioLength, Posts, HasProfilePic, IsPrivate, digitCount):
     
-    profile=instaloader.Profile.from_username(ig.context, usrname)
+    data = {
+        "FollowerCount": FollowerCount,
+        "FollowingCount": FollowingCount,
+        "BioLength": BioLength,
+        "Posts":  Posts,
+        "HasProfilePic": HasProfilePic,
+        "IsPrivate": IsPrivate,
+        "usernameDigitCount": digitCount
+    }
 
-    with open(r"./given_test.txt", 'w') as f:
-        sys.stdout = f
-        instaloader.Instaloader().download_profile(usrname, profile_pic_only=True)
-
-    sys.stdout = sys.stdout
-
-    input_file_name = r"./given_test.txt"
-    str_check= ""
-    with open(input_file_name, "r") as input_file:
-        line = input_file.readline()
-        str_check = input_file.read()
-
-    y = len(usrname) + 1
-    jh = "2018-11-21_19-35-46_UTC_profile_pic.jpg " + "\n"
-    check = str_check[y:]
-    profile_pic = 0
-
-    if check == jh:
-        profile_pic = 0
-    else:
-        profile_pic = 1
-
-    input_file.close()
-    shutil.rmtree(usrname)
-
-    x = 0
-    if profile.is_private:
-        x = 1
-    else:
-        x = 0
-
-    digit_count = sum(1 for char in profile.username if char.isdigit())
-
-    data = [{
-        "FollowerCount": int(profile.followers),
-        "FollowingCount": int(profile.followees),
-        "BioLength": len(profile.biography),
-        "Posts":  profile.mediacount,
-        "HasProfilePic": profile_pic,
-        "IsPrivate": x,
-        "usernameDigitCount": digit_count/len(profile.username)
-    }]
-
-    file_path = r"./given_user_json.json"
-    with open(file_path, 'w') as json_file:
-
-        json.dump(data, json_file)
-
-    with open(r"./given_user_json.json") as json_file:
-        jsondata = json.load(json_file)
-
-    data_file = open(r"./given_user_csv.csv", 'w', newline='')
-    csv_writer = csv.writer(data_file)
-
-    count = 0
-    for data in jsondata:
-        if count == 0:
-            header = data.keys()
-            csv_writer.writerow(header)
-            count += 1
-        csv_writer.writerow(data.values())
-    
     return data
        
 
@@ -91,10 +31,21 @@ def prediction(ans):
 def main():
         st.title("Fake Instagram Detector")
     
-        user = st.text_input("Enter the username")
-        
+        username = st.text_input("Enter the username")
+        userlength = len(username)
+        FollowerCount = st.number_input("Enter the Followers Count", min_value=1, value=None, step=1)
+        FollowingCount = st.number_input("Enter the Following Count", min_value=1, value=None, step=1)
+        BioLength = len(st.text_input("User Bio"))
+        Posts = st.slider("Number of Posts", 0, 2000)
+        HasProfilePic = st.number_input("Type 1 for profile pic else type 0", min_value=0, value=None, step=1)
+        IsPrivate = st.number_input("Type 1 for private acc else type 0", min_value=0, value=None, step=1)
+        digit_count = sum(1 for char in username if char.isdigit())
+
+        if userlength > 0:
+            usernameDigitCount = digit_count / userlength
+
         if st.button("Detect"):
-                data = scrap(user)
+                data = make_a_dict(FollowerCount, FollowingCount, BioLength, Posts, HasProfilePic, IsPrivate, usernameDigitCount)
 
                 ans = []
                 for keys in data:
@@ -108,6 +59,7 @@ def main():
                 st.markdown('<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">', unsafe_allow_html=True)
 
                 if fake == 0:     
+            
                     htmlstr = """
                     <style>
                     .green-tick {
